57f20955e24d09096a510e44a2591015fd19ddba
Thiago Farias <thiagofarias99@hotmail.com>
2020-05-15 15:25:10 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/thiagofarias/cursomc/domain/Cliente.java
+++ src/main/java/com/thiagofarias/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -107,6 +110,16 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
