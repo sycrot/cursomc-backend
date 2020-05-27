34137ecab1ae90d560b64b3b5de83c4aa2d34af6
Thiago Farias <thiagofarias99@hotmail.com>
2020-05-27 09:37:29 -0300

    Refresh token

--- src/main/java/com/thiagofarias/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/thiagofarias/cursomc/repositories/EnderecoRepository.java
@@ -3,7 +3,6 @@ package com.thiagofarias.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.thiagofarias.cursomc.domain.Categoria;
 import com.thiagofarias.cursomc.domain.Endereco;
 
 @Repository
