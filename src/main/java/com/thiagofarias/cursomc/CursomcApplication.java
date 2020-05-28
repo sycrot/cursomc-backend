9cbce88a3263ef4b65b6164cd0f4b88116d632f0
Thiago Farias <thiagofarias99@hotmail.com>
2020-05-28 10:02:27 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/thiagofarias/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagofarias/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.thiagofarias.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiagofarias.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	public void run(String... args) throws Exception {
 	
-		
+		s3service.uploadFile("C:\\Users\\thiag\\Pictures\\buzz.jpg");
 		
 	}
 
